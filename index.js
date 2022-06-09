45a529cbb6199d55f9e5449d818bc5ec47c85fef
ydennekrf <ydennekrf@gmail.com>
2022-06-09 16:39:51 -0400

    added questions and a function to pass the unique role question

--- index.js
+++ index.js
@@ -48,3 +48,72 @@ class intern extends developers{
         return this._school;
     }
 }
+
+const questions = [
+    {
+        type: 'list',
+        message: 'how many members in the team?',
+        name: 'numMembers',
+        choices: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9
+        ]
+    },
+    {
+        type: 'list',
+        message: 'What is your role?',
+        name: 'role',
+        choices: [
+            'manager',
+            'engineer',
+            'intern'
+        ]
+    },
+    {
+        type: 'input',
+        message: 'what is your name?',
+        name: 'name'
+    },
+    {
+        type: 'input',
+        message: 'what is the ID #?',
+        name: 'ID'
+    },
+    {
+        type: 'input',
+        message: 'what is the email?',
+        name: 'email'
+    }
+];
+
+function roleCheck (role) {
+    switch(role) {
+        case 'manager':
+            questions.next({
+                type: 'input',
+                message: 'What is the office number?',
+                name: 'office'
+            });
+        break;
+        case 'engineer':
+            questions.next({
+                type: 'input',
+                message: 'What is the github profile?',
+                name: 'github'
+            });
+        break;
+        case 'intern':
+            questions.next({
+                type: 'input',
+                message: 'What school are you from?',
+                name: 'school'
+            })
+    }
+};
